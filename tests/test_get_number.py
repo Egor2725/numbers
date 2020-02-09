bd95272b1509791f79717d6270ba35d73bdfad89
Egor Zuk <ggegor2736@gmail.com>
2020-02-09 21:05:58 +0300

    add logging

--- tests/test_get_number.py
+++ tests/test_get_number.py
@@ -1,14 +1,34 @@
 import pytest
 from core.views import find_prime_numbers
 
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+# def test_eggs():
+#     LOGGER.info('eggs info')
+#     LOGGER.warning('eggs warning')
+#     LOGGER.error('eggs error')
+#     LOGGER.critical('eggs critical')
+#     assert True
+
 
 def test_status_code(client):
-    response = client.get('/number/')
-    assert response.status_code
+    try:
+        response = client.get('/number/')
+        assert response.status_code
+        LOGGER.info('Test status code')
+    except:
+        LOGGER.error(f'Error in test status code')
 
 
 def test_check_number(client):
-    response = client.get('/number/')
-    number = response.json()['random_prime_number']
-    assert number in find_prime_numbers()
+    try:
+        response = client.get('/number/')
+        number = response.json()['random_prime_number']
+        assert number in find_prime_numbers()
+        LOGGER.info(f'Check number {number} is prime.')
+    except:
+        LOGGER.error(f'Error in test check number')
 
